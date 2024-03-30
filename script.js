f127e4b05b3a2de0e03e1feb0cbf6a05910bf2e3
subhasisadak <adaksubhasis2016@gmail.com>
2024-03-30 22:49:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let blur = document.getElementById("blur");
+let popup = document.getElementById("popup");
+let flag = 0;
+function toggle() {
+    if(flag == 0){
+        blur.classList.toggle('active');
+        console.log("done");
+        popup.classList.toggle('active');
+        // console.log(popup);
+        flag = 1;
+    }
+}
+function visible(){
+    blur.classList.toggle('active');
+    popup.classList.toggle('active');
+}
+window.onscroll = toggle;
+setTimeout(toggle, 20000);
